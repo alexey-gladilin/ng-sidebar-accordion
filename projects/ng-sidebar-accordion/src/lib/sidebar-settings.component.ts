607a679e3ecdea2a10738aafbb45c03123f1261a
alexey.gladilin <alexey.gladilin@yandex.ru>
2020-04-29 12:43:31 +0300

    feat: sidebar configuration component

---
+++ projects/ng-sidebar-accordion/src/lib/sidebar-settings.component.ts
@@ -0,0 +1,37 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+
+export type position = 'left' | 'top' | 'right' | 'bottom';
+export type mode = 'push' | 'over';
+
+@Component({
+  selector: 'ng-sidebar-settings',
+  template: '',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SidebarSettingsComponent {
+
+  @Output() modeChange = new EventEmitter<SidebarSettingsComponent>();
+  @Output() positionChange = new EventEmitter<SidebarSettingsComponent>();
+
+  private _position: position;
+
+  @Input() get position(): position {
+    return this._position;
+  };
+
+  set position(value: position) {
+    this._position = value;
+    this.positionChange.emit(this);
+  }
+
+  private _mode: mode;
+
+  @Input() get mode(): mode {
+    return this._mode;
+  }
+
+  set mode(value: mode) {
+    this._mode = value;
+    this.modeChange.emit(this);
+  }
+}
